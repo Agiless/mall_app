d0b73a5489696a12e21fe163523a744d3a4481ba
Deepakram007 <deepakramsekar@gmail.com>
2025-02-13 22:09:41 +0530

    dummy

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
+"use-client"
+import {Link} from "@mui/material";
+
+
 export default function App(){
+
+
   return(
   <>
-    <div>
-      <button>Log in</button>
+    <div className=" bg-[url(/malls-tenants-innovation-Simon-retail.webp)] bg-cover h-screen">
+      <nav>
+        <Link className=" px-6 py-2 rounded-md text-white shadow shadow-green-400 bg-green-600 ">Sign-up</Link>
+        <Link className = " ">Sign-up</Link>
+      </nav>
+      
+      <div className = "absolute top-0 left-[50vw] h-full">
+      <p className = "font-75">Smart Mall</p>
+      </div>
+      
+    
     </div>
+    
   </>)
 }
