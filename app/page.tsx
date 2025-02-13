ee21f2cb5f9b74e495417f72be39f8998e7b2d8f
Agiless <agilessshobika@gmail.com>
2025-02-13 21:01:56 +0530

    django added

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function App(){
   return(
   <>
     <div>
-      <Link href = "./login/seller" >Log in</Link>
+      <Link href = {"/login/seller"} className = " absolute right-10 bg-slate-400 px-4 py-2 rounded-[100px]" style = {{color:"black",textDecoration: "none"}}>Log in</Link>
       <Link href = "./login/customer" >Log iin</Link>
     </div>
   </>
