3ad54117a188d29ecd3060ef9592204690bf6fdf
Agiless <agilessshobika@gmail.com>
2025-02-13 21:36:36 +0530

    chatbot update

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,16 @@ import {Link} from "@mui/material";
 export default function App(){
   return(
   <>
+  <div>
+    <nav>
+      <Link href = {"/login/seller"} className = " relative top-4 left-[79vw] bg-slate-400 px-4 py-2 rounded-[100px]" style = {{color:"black",textDecoration: "none"}}>Log in</Link>
+      <Link href = {"/login/customer"} className = "relative top-4 left-[80vw] bg-slate-400 px-4 py-2 rounded-[100px]" style = {{color:"black",textDecoration: "none"}}>Log in</Link>
+    </nav>
     <div>
-      <Link href = {"/login/seller"} className = " absolute right-10 bg-slate-400 px-4 py-2 rounded-[100px]" style = {{color:"black",textDecoration: "none"}}>Log in</Link>
-      <Link href = "./login/customer" >Log iin</Link>
+      
     </div>
+  </div>
+    
   </>
   );
 }
